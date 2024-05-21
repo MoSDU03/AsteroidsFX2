460bdc6689492c7754761737274da94df9bf5e39
MoSDU03 <muhammedsdu22@gmail.com>
2024-05-21 16:39:23 +0200

    Spring implemented

--- Core/src/main/java/dk/sdu/mmmi/cbse/main/Main.java
+++ Core/src/main/java/dk/sdu/mmmi/cbse/main/Main.java
@@ -1,39 +1,10 @@
 package dk.sdu.mmmi.cbse.main;
 
-import dk.sdu.mmmi.cbse.common.data.Entity;
-import dk.sdu.mmmi.cbse.common.data.GameData;
-import dk.sdu.mmmi.cbse.common.data.GameKeys;
-import dk.sdu.mmmi.cbse.common.data.World;
-import dk.sdu.mmmi.cbse.common.services.IEntityProcessingService;
-import dk.sdu.mmmi.cbse.common.services.IGamePluginService;
-import dk.sdu.mmmi.cbse.common.services.IPostEntityProcessingService;
-import java.util.Collection;
-import java.util.Map;
-import java.util.ServiceLoader;
-import java.util.concurrent.ConcurrentHashMap;
-import static java.util.stream.Collectors.toList;
-
-import dk.sdu.mmmi.cbse.enemy.Enemy;
-import javafx.animation.AnimationTimer;
 import javafx.application.Application;
-import javafx.scene.Scene;
-import javafx.scene.input.KeyCode;
-import javafx.scene.layout.Pane;
-import javafx.scene.shape.Polygon;
-import javafx.scene.text.Text;
 import javafx.stage.Stage;
-import javafx.scene.paint.Color;
-import javafx.scene.shape.Rectangle;
-
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 public class Main extends Application {
-
-    private final GameData gameData = new GameData();
-    private final World world = new World();
-    private final Map<Entity, Polygon> polygons = new ConcurrentHashMap<>();
-    private final Pane gameWindow = new Pane();
-    private final Map<Entity, Rectangle> healthBars = new ConcurrentHashMap<>();
-
 
     public static void main(String[] args) {
         launch(Main.class);
@@ -41,143 +12,11 @@ public class Main extends Application {
 
     @Override
     public void start(Stage window) throws Exception {
-        Text text = new Text(10, 20, "Destroyed asteroids: 0");
-        gameWindow.setPrefSize(gameData.getDisplayWidth(), gameData.getDisplayHeight());
-        gameWindow.getChildren().add(text);
-
-        Scene scene = new Scene(gameWindow);
-        scene.setOnKeyPressed(event -> {
-            if (event.getCode().equals(KeyCode.LEFT)) {
-                gameData.getKeys().setKey(GameKeys.LEFT, true);
-            }
-            if (event.getCode().equals(KeyCode.RIGHT)) {
-                gameData.getKeys().setKey(GameKeys.RIGHT, true);
-            }
-            if (event.getCode().equals(KeyCode.UP)) {
-                gameData.getKeys().setKey(GameKeys.UP, true);
-            }
-            if (event.getCode().equals(KeyCode.SPACE)) {
-                gameData.getKeys().setKey(GameKeys.SPACE, true);
-            }
-        });
-        scene.setOnKeyReleased(event -> {
-            if (event.getCode().equals(KeyCode.LEFT)) {
-                gameData.getKeys().setKey(GameKeys.LEFT, false);
-            }
-            if (event.getCode().equals(KeyCode.RIGHT)) {
-                gameData.getKeys().setKey(GameKeys.RIGHT, false);
-            }
-            if (event.getCode().equals(KeyCode.UP)) {
-                gameData.getKeys().setKey(GameKeys.UP, false);
-            }
-            if (event.getCode().equals(KeyCode.SPACE)) {
-                gameData.getKeys().setKey(GameKeys.SPACE, false);
-            }
-        });
-
-        // Lookup all Game Plugins using ServiceLoader
-        System.out.println("Loading game plugins...");
-        for (IGamePluginService iGamePlugin : getPluginServices()) {
-            System.out.println("Starting plugin: " + iGamePlugin.getClass().getName());
-            iGamePlugin.start(gameData, world);
+        AnnotationConfigApplicationContext ctx = new AnnotationConfigApplicationContext(ModuleConfig.class);
+        for (String beanName : ctx.getBeanDefinitionNames()) {
+            System.out.println(beanName);
         }
-        System.out.println("Plugins started, initializing entities...");
-        initializeHealthBars();
-        for (Entity entity : world.getEntities()) {
-            Polygon polygon = new Polygon(entity.getPolygonCoordinates());
-            polygons.put(entity, polygon);
-            gameWindow.getChildren().add(polygon);
-            System.out.println("Entity added: " + entity.getID());
-            if (entity instanceof Enemy) {
-                Rectangle healthBar = new Rectangle(50, 5, Color.RED);
-                healthBars.put(entity, healthBar);
-                gameWindow.getChildren().add(healthBar);
-            }
-        }
-        System.out.println("Entities initialized.");
-        render();
-        window.setScene(scene);
-        window.setTitle("ASTEROIDS");
-        window.show();
-    }
-
-    private void render() {
-        new AnimationTimer() {
-            @Override
-            public void handle(long now) {
-                update();
-                draw();
-                gameData.getKeys().update();
-            }
-
-        }.start();
-    }
-
-    private void update() {
-        for (IEntityProcessingService entityProcessorService : getEntityProcessingServices()) {
-            entityProcessorService.process(gameData, world);
-        }
-        for (IPostEntityProcessingService postEntityProcessorService : getPostEntityProcessingServices()) {
-            postEntityProcessorService.process(gameData, world);
-        }
-    }
-
-    private void draw() {
-        for (Entity polygonEntity : polygons.keySet()) {
-            if (!world.getEntities().contains(polygonEntity)) {
-                Polygon removedPolygon = polygons.get(polygonEntity);
-                polygons.remove(polygonEntity);
-                gameWindow.getChildren().remove(removedPolygon);
-
-                if (polygonEntity instanceof Enemy) {
-                    Rectangle removedHealthBar = healthBars.get(polygonEntity);
-                    healthBars.remove(polygonEntity);
-                    gameWindow.getChildren().remove(removedHealthBar);
-                }
-            }
-        }
-
-        for (Entity entity : world.getEntities()) {
-            Polygon polygon = polygons.get(entity);
-            if (polygon == null) {
-                polygon = new Polygon(entity.getPolygonCoordinates());
-                polygons.put(entity, polygon);
-                gameWindow.getChildren().add(polygon);
-            }
-            polygon.setTranslateX(entity.getX());
-            polygon.setTranslateY(entity.getY());
-            polygon.setRotate(entity.getRotation());
-
-            if (entity instanceof Enemy) {
-                Rectangle healthBar = healthBars.get(entity);
-                if (healthBar == null) {
-                    healthBar = new Rectangle(50, 5, Color.RED);
-                    healthBars.put(entity, healthBar);
-                    gameWindow.getChildren().add(healthBar);
-                }
-                healthBar.setTranslateX(entity.getX() - 25);
-                healthBar.setTranslateY(entity.getY() - 35);
-                healthBar.setWidth(((Enemy) entity).getHealth() * 10);
-            }
-        }
-    }
-
-    private Collection<? extends IGamePluginService> getPluginServices() {
-        return ServiceLoader.load(IGamePluginService.class).stream().map(ServiceLoader.Provider::get).collect(toList());
-    }
-
-    private Collection<? extends IEntityProcessingService> getEntityProcessingServices() {
-        return ServiceLoader.load(IEntityProcessingService.class).stream().map(ServiceLoader.Provider::get).collect(toList());
-    }
-
-    private Collection<? extends IPostEntityProcessingService> getPostEntityProcessingServices() {
-        return ServiceLoader.load(IPostEntityProcessingService.class).stream().map(ServiceLoader.Provider::get).collect(toList());
-    }
-    private void initializeHealthBars() {
-        for (Entity entity : world.getEntities(Enemy.class)) {
-            Rectangle healthBar = new Rectangle(50, 5, Color.RED);
-            healthBars.put(entity, healthBar);
-            gameWindow.getChildren().add(healthBar);
-        }
+        Game game = ctx.getBean(Game.class);
+        game.start(window);
     }
 }
