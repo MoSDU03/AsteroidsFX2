c14b1bc97929b5cd99de23df8bceb64a16c0cd0d
MoSDU03 <muhammedsdu22@gmail.com>
2024-05-21 15:50:08 +0200

    Revert "Spring"
    This reverts commit bca6eb5332c0d93b501357fa0cdc084d53bea98a.

--- Core/src/main/java/module-info.java
+++ Core/src/main/java/module-info.java
@@ -1,13 +1,12 @@
 module Core {
     requires Common;
-    requires CommonBullet;
+    requires CommonBullet;    
     requires javafx.graphics;
     requires Enemy;
-    requires spring.context;
-    requires spring.beans;
-    requires spring.core;
-    opens dk.sdu.mmmi.cbse.main to javafx.graphics, spring.core;
+    opens dk.sdu.mmmi.cbse.main to javafx.graphics;
     uses dk.sdu.mmmi.cbse.common.services.IGamePluginService;
     uses dk.sdu.mmmi.cbse.common.services.IEntityProcessingService;
     uses dk.sdu.mmmi.cbse.common.services.IPostEntityProcessingService;
 }
+
+
